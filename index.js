263a2b0aec129bdf71bd844578a468da7e49cba1
Elizabeth Doyle <myoddjourney@gmail.com>
2023-02-01 12:19:16 -0500

    8 out of 8 passing tests

--- index.js
+++ index.js
@@ -181,10 +181,18 @@ Use the filterByWord function below to do the following:
 */
 
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
-}
+function filterByWord(array, string){
+  let filteredArray = [];
 
+  for(let i = 0; i < array.length; i++) {
+    if(array[i].includes(string)) {
+         filteredArray.push(array[i]) 
+      } 
+  }
+   return filteredArray
+  }
+
+console.log(filterByWord(originalFlavors, "Chocolate", "task 7"))
 
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
